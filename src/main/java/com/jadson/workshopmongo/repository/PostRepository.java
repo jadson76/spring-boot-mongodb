3678aab94afae8ff70d9dc4969e3ba313a82408b
jadson76 <biojadson@gmail.com>
2021-09-16 23:39:06 -0300

    consulta com varios criterios

--- src/main/java/com/jadson/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jadson/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.jadson.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jadson.workshopmongo.domain.Post;
@@ -11,5 +13,11 @@ import com.jadson.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title' : { $regex: ?0, $option: 'i'}}")
+	List<Post> searchByTitle(String text);
+	
+	@Query("{ $and: [{date: {$gte: ?1} }, {date: {$lte: ?2}}, { $or: [{ 'title' : { $regex: ?0, $option: 'i'}},{ 'body' : { $regex: ?0, $option: 'i'}},{ 'comments.text' : { $regex: ?0, $option: 'i'}}] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
